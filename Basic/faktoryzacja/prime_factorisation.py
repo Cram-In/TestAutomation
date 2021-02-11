735e0aaac553e4c446b12db17105c8afaaaf9221
Marcin <bizior.marcin@googlemail.com>
2021-02-11 16:05:10 +0100

    completed pytest, tests error fix, logic order of operations changed

--- Basic/faktoryzacja/prime_factorisation.py
+++ Basic/faktoryzacja/prime_factorisation.py
@@ -1,8 +1,21 @@
 def prime_factors(num):
     factors = []
+    i = 2
+
     if type(num) != int:
-        raise ValueError()
-    if num % 2 == 0:
-        return True
-    else:
-        raise ValueError()
+        raise ValueError("illegal symbols used")
+
+    if num % 2 != 0:
+        factors.append(num)
+        raise ValueError(num, "is a prime number")
+
+    while i * i <= num:
+        if num % i:
+            i += 1
+        else:
+            num //= i
+            factors.append(i)
+    if num > 1:
+        factors.append(num)
+
+    return factors
--- Basic/faktoryzacja/pytest_prime_factors.py
+++ Basic/faktoryzacja/pytest_prime_factors.py
@@ -14,3 +14,13 @@ def test_illegal_symbols():
 def test_num_as_prime():
     with pytest.raises(ValueError):
         prime_factors(47)
+
+
+def test_factors():
+    result = prime_factors(86)
+    assert result == [2, 43]
+
+
+def test_prime_factors_task():
+    result = prime_factors(3958159172)
+    assert result == [2, 2, 11, 2347, 38329]
