fda4940a5eef6014f3f554d41d1b5289ae53d150
Marcin <bizior.marcin@googlemail.com>
2021-02-11 12:02:26 +0100

    illigal num test fix

--- Basic/faktoryzacja/prime_factorisation.py
+++ Basic/faktoryzacja/prime_factorisation.py
@@ -1,3 +1,8 @@
 def prime_factors(num):
     factors = []
-    pass
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            raise ValueError(num, "is a prime number")
