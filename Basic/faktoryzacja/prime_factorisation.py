56c2610d11a1f7f1f3d2dd7a2919d66ad37f8f88
Marcin <bizior.marcin@googlemail.com>
2021-02-11 13:59:16 +0100

    re-set

--- Basic/faktoryzacja/prime_factorisation.py
+++ Basic/faktoryzacja/prime_factorisation.py
@@ -1,22 +1,2 @@
-from functools import wraps
-
-
-def validate_number(function):
-    @wraps(function)
-    def wrapper(num):
-        if num != int:
-            raise ValueError("illegal symbol")
-        if num > 1:
-            for i in range(2, num):
-                if (num % i) == 0:
-                    factors.append(i)
-                    break
-            else:
-                raise ValueError(num, "is a prime number")
-
-    return wrapper
-
-
-@validate_number
 def prime_factors(num):
     factors = []
