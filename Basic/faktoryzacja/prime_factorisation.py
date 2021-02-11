9f972df4220fd72f2395439f5ccdd10432ccb5a3
Marcin <bizior.marcin@googlemail.com>
2021-02-11 12:15:01 +0100

    wrapper added, new test for illegal symbols created

--- Basic/faktoryzacja/prime_factorisation.py
+++ Basic/faktoryzacja/prime_factorisation.py
@@ -1,8 +1,20 @@
+from functools import wraps
+
+
+def validate_number(function):
+    @wraps(function)
+    def wrapper(num):
+        if num > 1:
+            for i in range(2, num):
+                if (num % i) == 0:
+                    factors.append(i)
+                    break
+            else:
+                raise ValueError(num, "is a prime number")
+
+    return wrapper
+
+
+@validate_number
 def prime_factors(num):
     factors = []
-    if num > 1:
-        for i in range(2, num):
-            if (num % i) == 0:
-                break
-        else:
-            raise ValueError(num, "is a prime number")
--- Basic/faktoryzacja/test_prime_factors.py
+++ Basic/faktoryzacja/test_prime_factors.py
@@ -3,10 +3,10 @@ from prime_factorisation import prime_factors
 
 def test_prime_factors():
     try:
-        from prime_factorisation import prime_factors  # ➜ 1
+        from prime_factorisation import prime_factors
 
-        assert callable(prime_factors), "prime_factors not callable"  # ➜ 2
-    except ImportError as error:  # ➜ 3
+        assert callable(prime_factors), "prime_factors not callable"
+    except ImportError as error:
         assert False, error
 
 
@@ -18,11 +18,20 @@ def test_num_as_prime():
         pass
 
 
+def test_illegal_symbols():
+    try:
+        prime_factors("screwdriver")
+        assert False, "ValueError expected"
+    except ValueError:
+        pass
+
+
 if __name__ == "__main__":
     for test in (
         test_prime_factors,
         test_num_as_prime,
-    ):  # ➜ 4
+        test_illegal_symbols,
+    ):
         print(f"{test.__name__}: ", end="")
         try:
             test()
