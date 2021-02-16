8d0d8001c03344debe5daf4c920e8a03cf2cf056
Marcin <bizior.marcin@googlemail.com>
2021-02-16 13:27:28 +0100

    test solution implementede, utilities file created

--- conversion/src/converter/convert_inputs.py
+++ conversion/src/converter/convert_inputs.py
@@ -1,2 +1,8 @@
+from converter.utilities import *
+
+
 def convert_to_roman(num):
-    pass
+    if int(num) != num:
+        raise ValueError("Not integer")
+    if not (0 < num < 5000):
+        raise OutOfRangeError("number out of range (must be 1..4999)")
--- conversion/tests/test_converter.py
+++ conversion/tests/test_converter.py
@@ -2,9 +2,14 @@ import unittest
 import pytest
 
 from converter.convert_inputs import convert_to_roman
+from converter.utilities import *
 
 
-class TestRangeConvertion(unittest.TestCase):
-    def number_out_of_range(self):
-        with self.assertRaises(OutOfRangeError):
-            convert_to_roman(5000)
+def test_number_out_of_range():
+    with pytest.raises(OutOfRangeError):
+        convert_to_roman(5000)
+
+
+def test_number_not_ingeger():
+    with pytest.raises(ValueError):
+        convert_to_roman("Ala Ma Kota")
