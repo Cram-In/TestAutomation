175bc26e6458f3833a8861345c39a1b41e6e1ec5
Marcin <bizior.marcin@googlemail.com>
2021-02-16 13:59:09 +0100

    test solution implemented, tests screenshot added

--- conversion/src/converter/convert_inputs.py
+++ conversion/src/converter/convert_inputs.py
@@ -1,8 +1,31 @@
 from converter.utilities import *
 
+numeralMap = (
+    ("M", 1000),
+    ("CM", 900),
+    ("D", 500),
+    ("CD", 400),
+    ("C", 100),
+    ("XC", 90),
+    ("L", 50),
+    ("XL", 40),
+    ("X", 10),
+    ("IX", 9),
+    ("V", 5),
+    ("IV", 4),
+    ("I", 1),
+)
+
 
 def convert_to_roman(num):
     if int(num) != num:
         raise ValueError("Not integer")
     if not (0 < num < 5000):
         raise OutOfRangeError("number out of range (must be 1..4999)")
+
+    result = ""
+    for roman, arabic in numeralMap:
+        while num >= arabic:
+            result += roman
+            num -= arabic
+    return result
--- conversion/tests/test_converter.py
+++ conversion/tests/test_converter.py
@@ -4,6 +4,8 @@ import pytest
 from converter.convert_inputs import convert_to_roman
 from converter.utilities import *
 
+test_cases = {5: "V", 4: "IV", 9: "IX", 11: "XI", 90: "XC", 110: "CX", 777: "DCCLXXVII"}
+
 
 def test_number_out_of_range():
     with pytest.raises(OutOfRangeError):
@@ -13,3 +15,9 @@ def test_number_out_of_range():
 def test_number_not_ingeger():
     with pytest.raises(ValueError):
         convert_to_roman("Ala Ma Kota")
+
+
+class TestFinalConvertion(unittest.TestCase):
+    def test_converting_to_roman(self):
+        for num, result in test_cases.items():
+            self.assertEqual(convert_to_roman(num), result)
