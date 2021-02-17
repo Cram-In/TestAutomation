cb1bbafc675f0468df1a35ea2ccba43dde3993fd
Marcin <bizior.marcin@googlemail.com>
2021-02-17 22:50:51 +0100

    repo cleaning

--- tic_tac_toe/start.sh
+++
@@ -1,6 +0,0 @@
-export FLASK_APP=api.py
-export FLASK_ENV=development
-set FLASK_ENV=development
-set FLASK_APP=api.py
-source /home/the-barbarian/PycharmProjects/TestAutomation/virtualenv/bin
-flask run
