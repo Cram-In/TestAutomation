5eb91bfaa9f069e34349d9fb0a3be9f0c1d75511
Marcin <bizior.marcin@googlemail.com>
2021-02-19 10:51:23 +0100

    data base tests mock and fixtures

--- tic_tac_toe/tests/test_database.py
+++ tic_tac_toe/tests/test_database.py
@@ -3,39 +3,40 @@ from os import close, unlink
 from tempfile import mkstemp
 from itertools import count
 from sqlalchemy import create_engine
+from unittest.mock import Mock
 
 from tic_tac_toe.database import winner, metadata, history
 
 
-@pytest.fixture
-def create_games(database_connection):
-    def game_creator(*games):
-        move_id = count(1)
-        for game_id, moves in enumerate(games, 1):
-            database_connection.execute(
-                history.insert(),
-                [
-                    {"game_id": game_id, "move_id": next(move_id), "position": int(position), "symbol": symbol}
-                    for position, symbol in zip(moves[::2], moves[1::2])
-                ],
-            )
-
-    return game_creator
+User = Mock(query=Mock(all=Mock(return_value=[Mock(id=7, first_name="Guido", last_name="van Rossum")])))
 
 
 @pytest.fixture
-def database_connection():
-    fd, name = mkstemp(prefix="test_winner_", suffix=".sqlite")
-    engine = create_engine(f"sqlite:///{name}")
-    metadata.create_all(engine)
-    with engine.connect() as connection:
-        yield connection
-    close(fd)
-    unlink(name)
+def connection_mock():
+    return lambda moves: Mock(
+        execute=Mock(return_value=[(int(position), symbol) for position, symbol in zip(moves[::2], moves[1::2])])
+    )
 
 
-def test_3x_in_a_column(database_connection, create_games):
-    response = winner(database_connection, 1)
-    print(dir(response))
-    create_games("4X1O5X3O2X6O8X")
-    assert winner(database_connection, 1) == "X"
+def test_3x_in_a_column(connection_mock):
+    response = winner(connection_mock("4X1O5X3O2X6O8X"), None)
+    assert response.json()["winner"] == "X"
+
+
+@pytest.mark.parametrize(
+    "board, expected",
+    [("3O0X4O1X7O2X", "X"), ("0O3X2O4X7O5X", "X"), ("0O6X2O7X3O8X", "X"), ("4O1X5O3X2O6X8O", "O")],
+)
+def test_3x_in_a_row(connection_mock, expected, board):
+    response = winner(connection_mock(board), None)
+    assert response.json()["winner"] == expected
+
+
+def test_create_board_from_moves(connection_mock, monkeypatch):
+    monkeypatch.setattr("tic_tac_toe.database.tic_tac_toe_winner", lambda board: board)
+    response = winner(connection_mock("4X1O5X3O2X6O8X"), None)
+    print("Response type: ", type(response))
+    print("Assertion right side type: ", type(" OXOXXO X"))
+    print("Are they equal?")
+    print(f"assert response:", response.json())
+    assert response == " OXOXXO X"
